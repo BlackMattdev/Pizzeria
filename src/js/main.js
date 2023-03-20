926c2b3423c37b67926c0b7776c81023a2a15591
Mateusz <blackmattfrontend@gmail.com>
2023-03-20 23:43:00 +0100

    Create js for year in footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,19 @@
 const hamburger = document.querySelector('.navbar__hamburger');
 const navmobile = document.querySelector('.navbar__links');
 
+const footerYear = document.querySelector('.footer__year');
+
 const handleNav = () => {
 	navmobile.classList.toggle('navbar__links--active');
     console.log('test');
 };
 
+
+
+const handleCurrentYear = () => {
+    const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
 hamburger.addEventListener('click', handleNav);
