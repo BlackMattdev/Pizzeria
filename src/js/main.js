f08286a1d3b29ac0f7b4772994c2cc0e6f90822c
Mateusz <blackmattfrontend@gmail.com>
2023-04-12 23:35:00 +0200

    Adding javascript to block hamburger menu scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,24 @@
 const hamburger = document.querySelector('.navbar__hamburger');
 const navmobile = document.querySelector('.navbar__links');
-const navbar = document.querySelector('.navbar')
+const navbar = document.querySelector('.navbar');
 const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
 	navmobile.classList.toggle('navbar__links--active');
-    console.log('test');
+	navmobile.classList.toggle('active');
+	document.body.classList.toggle('sticky-body');
 };
 
-
-
 const handleCurrentYear = () => {
-    const year = new Date().getFullYear();
+	const year = new Date().getFullYear();
 	footerYear.innerText = year;
 };
 
-
 const handleNavBgc = () => {
-    navbar.classList.toggle('navbar--active', window.scrollY > 0)
-}
+	navbar.classList.toggle('navbar--active', window.scrollY > 0);
+};
 
 
 handleCurrentYear();
 hamburger.addEventListener('click', handleNav);
-window.addEventListener('scroll', handleNavBgc)
+window.addEventListener('scroll', handleNavBgc);
